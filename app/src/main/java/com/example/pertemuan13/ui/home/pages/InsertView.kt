b432696fb2b35300e1222a13571c1f6b9dffecab
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2025-01-10 20:21:53 +0700

    menambahkan tata letak Scaffold

--- app/src/main/java/com/example/pertemuan13/ui/home/pages/InsertView.kt
+++ app/src/main/java/com/example/pertemuan13/ui/home/pages/InsertView.kt
@@ -1,11 +1,20 @@
 package com.example.pertemuan13.ui.home.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan13.ui.PenyediaViewModel
 import com.example.pertemuan13.ui.home.viewmodel.FormState
@@ -51,4 +60,17 @@ fun InsertMhsView(
             else -> Unit
         }
     }
-}
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title = { Text("Tambah Mahasiswa") },
+                navigationIcon = {
+                    Button(onClick = onBack) {
+                        Text("Back")
+                    }
+                }
+            )
+        }
