3f02396294ea0412cf8b129d8bf0773bd960eccd
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2025-01-10 19:58:50 +0700

    buat fungsi komposisi InsertMhsView dengan navigasi dan inisialisasi viewModel

---
+++ app/src/main/java/com/example/pertemuan13/ui/home/pages/InsertView.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan13.ui.home.pages
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan13.ui.PenyediaViewModel
+import com.example.pertemuan13.ui.home.viewmodel.InsertViewModel
+
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
