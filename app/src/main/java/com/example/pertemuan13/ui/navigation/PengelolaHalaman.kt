bdf5d2cf6e16b66bee770abc4c619407c0e98eb6
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2025-01-11 19:11:39 +0700

    menambahkan navigasi antara HomeScreen dan InsertMhsView

---
+++ app/src/main/java/com/example/pertemuan13/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,41 @@
+package com.example.pertemuan13.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pertemuan13.ui.home.pages.InsertMhsView
+
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan13.ui.home.pages.HomeScreen
+import com.example.pertemuan13.ui.home.viewmodel.HomeViewModel
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        
+        composable(DestinasiInsert.route){
+            InsertMhsView(
+                onBack = {navController.popBackStack()},
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
